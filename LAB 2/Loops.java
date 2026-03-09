30a7c4d18373237ac65ebb07c1a26b6dfee8f371
tejashwini666 <kandhaditejashwinireddy@gmail.com>
2026-03-09 22:37:09 +0530

    Add multiplication table program using loops

---
+++ LAB 2/Loops.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Main1{
+	public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);int n=sc.nextInt();
+        int choice=0;
+	int i=1;
+	Outerwhile: while(i<=n){ 
+	               for(int j=1;j<=10;j++){
+			System.out.println(i+"x"+j+"="+i*j);
+			if (j>=10){
+			System.out.println("If you want to continue please enter 1 otherwise 0");
+			choice=sc.nextInt();
+			switch(choice){
+				case 0: break Outerwhile;
+				case 1: continue Outerwhile;
+				}
+			}
+			i++;
+        
+
+			}
+		}
+	}
+}
